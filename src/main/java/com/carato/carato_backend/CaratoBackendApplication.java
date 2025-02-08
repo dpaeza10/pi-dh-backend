fe488618c0a19eb8df59a865bd715982d0fe2d6b
Jesus Mendoza <ccredulidad@gmail.com>
2025-02-08 18:13:17 -0500

    Initial springboot project

---
+++ src/main/java/com/carato/carato_backend/CaratoBackendApplication.java
@@ -0,0 +1,13 @@
+package com.carato.carato_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaratoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaratoBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/carato/carato_backend/CaratoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carato.carato_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaratoBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
